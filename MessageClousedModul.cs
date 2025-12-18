4a045aea2cf9291be4bbea3c868703ca538079aa
12 <1@DESKTOP-NN47RQP>
2025-12-18 17:19:56 +0200

    asd

---
+++ MessageClousedModul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryForSignalR
+{
+    public class MessageClousedModul
+    {
+        public string ModulName { get; set; }
+        public string Message { get; set; }
+        public MessageClousedModul(string ModulName, string Message  )
+        {
+            this.ModulName = ModulName;
+
+            this.Message = Message;
+        }
+    }
+}
